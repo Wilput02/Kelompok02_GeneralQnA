a6f7d73919f2eb233208954f9917e82725da1b47
CarloAgastya <carloagastya.ca@gmail.com>
2022-12-13 00:20:57 +0800

    up documen ready

--- public/js/qna.js
+++ public/js/qna.js
@@ -57,3 +57,16 @@ function validate() {
    }
  });
 }
+
+$(document).ready(function () {
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    });
+  
+    $(".form-qna").submit(function (e) {
+      e.preventDefault();
+      validate();
+    });
+  })
