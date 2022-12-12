51b6f8223ba8cc1fd54c1a2bac528d9b84a77502
CarloAgastya <carloagastya.ca@gmail.com>
2022-12-13 00:30:06 +0800

    up buat routes 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('qna');
-});
+});*/
+
+Route::get('/', [QnaCtrl::class, 'index']);
