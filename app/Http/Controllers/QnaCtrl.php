5238bfb5cb8de8556e565e85a9af3150283c6592
CarloAgastya <carloagastya.ca@gmail.com>
2022-12-13 00:26:56 +0800

    up kontroller qna 1

--- app/Http/Controllers/QnaCtrl.php
+++ app/Http/Controllers/QnaCtrl.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class QnaCtrl extends Controller
 {
     //
+    public function index()
+    {
+        return view('qna', [
+            'title' => 'QNA Page',
+            'active' => 'QNA'
+        ]);
+    }
 }
